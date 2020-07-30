ad9fb018b4f14306a9e6fdf8ae45c4ee9707c2e6
Champrin <29009876+Champrin@users.noreply.github.com>
2020-07-31 02:56:39 +0800

    Add files via upload

--- src/xyz/champrin/simplegame/games/OreRace.java
+++ src/xyz/champrin/simplegame/games/OreRace.java
@@ -28,7 +28,7 @@ public class OreRace extends Games implements Listener {
         put(Block.REDSTONE_ORE, 10);
         put(Block.DIAMOND_ORE, 20);
         put(Block.EMERALD_ORE, 30);
-        put(Block.DIRT, 1);
+        put(Block.STONE, 1);
     }};
 
     @EventHandler(priority = EventPriority.MONITOR)
@@ -46,59 +46,37 @@ public class OreRace extends Games implements Listener {
         }
     }
 
-    @EventHandler(priority = EventPriority.MONITOR)
-    public void onTouch(PlayerInteractEvent event) {
-        if (room.gameType.equals("OreRace")) {
-            Player player = event.getPlayer();
-            if (room.gamePlayer.contains(player)) {
-                int blockId = event.getBlock().getId();
-                int inHandId = player.getInventory().getItemInHand().getId();
-                if (blockId == Block.DIAMOND_BLOCK) {
-                    gameFinish(player);
-                } else if (inHandId == Item.ARROW) {
-                    player.getInventory().removeItem(new Item(262, 0, 1));
-                    String[] p = ((String) room.data.get("pos1")).split("\\+");
-                    player.teleport(new Vector3(Integer.parseInt(p[0]), Integer.parseInt(p[1]), Integer.parseInt(p[2])));
-                }
-            }
-        }
-    }
-
     public void madeArena() {
         Level level = room.level;
-        int v = 14;
-        if ((room.xa - room.xi) != 0) {
-            v = Math.abs(room.xa - room.xi) * 14;
-        } else if ((room.yi - room.ya) != 0) {
-            v = 14 * Math.abs(room.ya - room.yi);
-        }
+        int v = Math.abs(room.xa - room.xi) * 12 * (Math.abs(room.ya - room.yi));
+
         for (int x = room.xi; x <= room.xa; x++) {
             for (int z = room.zi; z <= room.za; z++) {
-                for (int y = room.yi; y < room.yi + 15; y++) {
+                for (int y = room.yi; y < room.yi + 13; y++) {
                     level.setBlock(new Vector3(x, y+1, z), Block.get(1, 0));//STONE
                 }
             }
         }
         for (int i = 0; i <= v / 3; i++) {
-            level.setBlock(room.getRandPos(15), Block.get(16, 0));//煤
+            level.setBlock(room.getRandPos(13), Block.get(16, 0));//煤
         }
         for (int i = 0; i <= v / 3; i++) {
-            level.setBlock(room.getRandPos(15), Block.get(14, 0));//GOLD
+            level.setBlock(room.getRandPos(13), Block.get(14, 0));//GOLD
         }
         for (int i = 0; i <= v / 3; i++) {
-            level.setBlock(room.getRandPos(15), Block.get(15, 0));//IRON
+            level.setBlock(room.getRandPos(13), Block.get(15, 0));//IRON
         }
         for (int i = 0; i <= v / 3; i++) {
-            level.setBlock(room.getRandPos(15), Block.get(21, 0));//LAPIS
+            level.setBlock(room.getRandPos(13), Block.get(21, 0));//LAPIS
         }
         for (int i = 0; i <= v / 3; i++) {
-            level.setBlock(room.getRandPos(15), Block.get(73, 0));//REDSTONE
+            level.setBlock(room.getRandPos(13), Block.get(73, 0));//RedStone
         }
         for (int i = 0; i <= v / 3; i++) {
-            level.setBlock(room.getRandPos(15), Block.get(56, 0));//钻石
+            level.setBlock(room.getRandPos(13), Block.get(56, 0));//钻石
         }
         for (int i = 0; i <= v / 3; i++) {
-            level.setBlock(room.getRandPos(15), Block.get(129, 0));//绿宝石
+            level.setBlock(room.getRandPos(13), Block.get(129, 0));//绿宝石
         }
         finishBuild();
     }
